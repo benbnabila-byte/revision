1a454cfbe4a694572ed6720703561b52e605b4ad
benbnabila-byte <benb.nabila@gmail.com>
2026-03-18 20:38:52 -0500

    Initial commit

---
+++ mod_main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return  'Bienvenue monde des totos'
+
+@app.route('/produit/<titre>', methods=['GET'])
+def produtto(titre):
+    return f'Produit:{titre}'
+
+@app.route('/acme/produit/<titre>')
+def prod(titre):
+    return render_template('produit.html', titulo=titre)
+
+if __name__ == '__main__':
+    app.run(debug=True)
