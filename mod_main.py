661f35239e6e3368fd2db779f0929bcb3b60fd47
benbnabila-byte <benb.nabila@gmail.com>
2026-03-18 21:16:00 -0500

    mise a jour3

--- mod_main.py
+++ mod_main.py
@@ -15,4 +15,4 @@ def prod(titre):
     return render_template('produit.html', titulo=titre)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
